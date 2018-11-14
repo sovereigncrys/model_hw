0a408f5ad713e96ffc6046729a7e35373e2e10e5
Crys <chsantiago4@gmail.com>
2018-11-13 21:09:21 -0700

    finally! the final solution

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,20 +4,20 @@ class PeopleController < ApplicationController
   end
 
   def show
-    @people = Person.find(params[:id])
+    @person = Person.find(params[:id])
   end
 
   def new
-    @people = Person.new
+    @person = Person.new
   end
 
   def edit
-    @people = Person.find(params[:id])
+    @person = Person.find(params[:id])
   end
 
   def create
-    @people = Person.new(people_params)
-    if(@people.save)
+    @person = Person.new(person_params)
+    if(@person.save)
       redirect_to person_path
     else
       render :new
@@ -25,22 +25,22 @@ class PeopleController < ApplicationController
   end
 
     def update
-      @people = Person.find(params[:id])
+      @person = Person.find(params[:id])
       
-      if @people.update(people_params)
+      if @person.update(person_params)
         redirect_to person_path
       else
         render :edit
       end
   end
   def destroy
-    @people = Person.find(params[:id]).destroy
+    @person = Person.find(params[:id]).destroy
     redirect_to person_path
   end
 
   private
-  def people_params
-    params.require(:people).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
+  def person_params
+    params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
   end
 
 end
